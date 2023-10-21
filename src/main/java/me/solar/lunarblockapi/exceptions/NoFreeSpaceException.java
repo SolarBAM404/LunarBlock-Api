1e24b3bc8c7b1ad1fffddfa5caf97fa4f4fcb3f4
Solar <solar+git@lunar-dev.com>
2023-10-22 00:38:09 +0100

    :sparkles: Added a custom item class to help with create custom items with unique data
    This will be expanding further to provide more support for different things but for now it just supports adding data to the items metadata
    
    Took 2 hours 2 minutes
    
    Took 49 seconds

---
+++ src/main/java/me/solar/lunarblockapi/exceptions/NoFreeSpaceException.java
@@ -0,0 +1,7 @@
+package me.solar.lunarblockapi.exceptions;
+
+public class NoFreeSpaceException extends Exception {
+    public NoFreeSpaceException(String message) {
+        super(message);
+    }
+}
