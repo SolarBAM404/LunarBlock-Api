8d34e0efc1cc396584d47520a61131cd9b6c5513
Solar <solar+git@lunar-dev.com>
2023-10-22 23:38:50 +0100

    :sparkles: create a RunnableObject for easily creating and running tasks
    Took 31 minutes

---
+++ src/main/java/com/lunardev/lunarlib/tasks/RunnableObject.java
@@ -0,0 +1,35 @@
+package com.lunardev.lunarlib.tasks;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public abstract class RunnableObject {
+
+    private int taskId = -1;
+
+    public abstract void action();
+
+    private Runnable getRunnable() {
+        return this::action;
+    }
+
+    public void startTask(JavaPlugin plugin, long delay) {
+        plugin.getServer().getScheduler()
+                .scheduleSyncDelayedTask(plugin, getRunnable(), delay * 20);
+    }
+
+    public void startRepeating(JavaPlugin plugin, long initialDelay, long repeatingDelay) {
+        if (taskId == -1) {
+            taskId = plugin.getServer().getScheduler()
+                    .scheduleSyncRepeatingTask(plugin, getRunnable(), initialDelay, repeatingDelay);
+        }
+    }
+
+    public void cancelTask() {
+        if (taskId != -1) {
+            Bukkit.getServer().getScheduler().cancelTask(taskId);
+            taskId = -1;
+        }
+    }
+
+}
