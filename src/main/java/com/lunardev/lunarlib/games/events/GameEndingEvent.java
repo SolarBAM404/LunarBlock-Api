d6833a74c12e481f2c9c1376c36ecc4ad5160b05
Solar <solar+git@lunar-dev.com>
2023-10-26 18:55:38 +0100

    :sparkles: game start/end events added
    Took 1 hour 19 minutes

---
+++ src/main/java/com/lunardev/lunarlib/games/events/GameEndingEvent.java
@@ -0,0 +1,15 @@
+package com.lunardev.lunarlib.games.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class GameEndingEvent extends Event {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+}
