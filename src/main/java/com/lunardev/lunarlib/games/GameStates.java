61b6f961fa4f68904c6469a3a17dab8ad42340d4
Solar <solar+git@lunar-dev.com>
2023-10-26 18:57:25 +0100

    :sparkles: added game states
    Took 2 minutes

---
+++ src/main/java/com/lunardev/lunarlib/games/GameStates.java
@@ -0,0 +1,16 @@
+package com.lunardev.lunarlib.games;
+
+/**
+ * The State enum class represents the different states of the game,
+ * such as 'WAITING', 'STARTED', and 'ENDING', allowing for easy management and control of game flow.
+ */
+public enum GameStates {
+
+    WAITING,
+    STARTING,
+    STARTED,
+    PLAYING,
+    ENDING,
+    ENDED
+
+}
