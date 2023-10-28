716c73f8b78a366ab5d486110bfc38b8aacb85a7
Solar <solar+git@lunar-dev.com>
2023-10-28 14:18:31 +0100

    :sparkles: added timer field and start/end game methods
    Took 10 minutes

--- src/main/java/com/lunardev/lunarlib/games/GameStateManager.java
+++ src/main/java/com/lunardev/lunarlib/games/GameStateManager.java
@@ -1,5 +1,8 @@
 package com.lunardev.lunarlib.games;
 
+import com.lunardev.lunarlib.games.events.GameEndEvent;
+import com.lunardev.lunarlib.games.events.GameStartEvent;
+import com.lunardev.lunarlib.tasks.RunnableObject;
 import lombok.Data;
 
 @Data
@@ -8,5 +11,16 @@ public class GameStateManager {
     private GameStates state;
     private Arena arena;
 
+    private RunnableObject matchTimer;
 
+    public void startGame() {
+        state = GameStates.PLAYING;
+        new GameStartEvent(this).callEvent();
+    }
+
+
+    public void endGame() {
+        new GameEndEvent(this).callEvent();
+        state = GameStates.ENDED;
+    }
 }
