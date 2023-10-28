f450b5b60e0d1ef511a1360e8364f341ead1b131
Solar <solar+git@lunar-dev.com>
2023-10-28 12:45:51 +0100

    :fire: Removed unused game states
    Took 31 minutes

--- src/main/java/com/lunardev/lunarlib/games/GameStates.java
+++ src/main/java/com/lunardev/lunarlib/games/GameStates.java
@@ -2,15 +2,12 @@ package com.lunardev.lunarlib.games;
 
 /**
  * The State enum class represents the different states of the game,
- * such as 'WAITING', 'STARTED', and 'ENDING', allowing for easy management and control of game flow.
+ * such as 'WAITING', 'PLAYING', and 'ENDED', allowing for easy management and control of game flow.
  */
 public enum GameStates {
 
     WAITING,
-    STARTING,
-    STARTED,
     PLAYING,
-    ENDING,
     ENDED
 
 }
