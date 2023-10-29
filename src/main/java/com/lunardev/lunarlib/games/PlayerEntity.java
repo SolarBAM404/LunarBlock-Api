6c8dfcff7ba0d5915fbf3c8bcda1a2c4a0572d8c
Solar <solar+git@lunar-dev.com>
2023-10-29 01:56:45 +0100

    :sparkles: added player field to PlayerEntity
    Took 1 hour 3 minutes

--- src/main/java/com/lunardev/lunarlib/games/PlayerEntity.java
+++ src/main/java/com/lunardev/lunarlib/games/PlayerEntity.java
@@ -1,15 +1,19 @@
 package com.lunardev.lunarlib.games;
 
 import lombok.Data;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 @Data
 public class PlayerEntity {
 
     private String playerName;
+    private Player player;
     private int score = 0;
 
     public PlayerEntity(String playerName) {
         this.playerName = playerName;
+        player = getPlayer();
     }
 
     public void addScore(int points) {
@@ -18,5 +22,9 @@ public class PlayerEntity {
 
     public void removeScore(int points) {
         score -= points;
+    }
+
+    public Player getPlayer() {
+        return Bukkit.getPlayer(playerName);
     }
 }
