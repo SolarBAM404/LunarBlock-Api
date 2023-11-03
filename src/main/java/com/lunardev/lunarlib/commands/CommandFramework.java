c9515c7ee0581162664f947e2ff6fe6a548f0088
Solar <solar+git@lunar-dev.com>
2023-11-03 18:18:10 +0000

    :sparkles: Changed tab complete along with how the arguments work
    This was done as to make it easier to add/work with the arguments
    
    Took 58 seconds

--- src/main/java/com/lunardev/lunarlib/commands/CommandFramework.java
+++ src/main/java/com/lunardev/lunarlib/commands/CommandFramework.java
@@ -11,9 +11,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.List;
+import java.util.*;
 
 /**
  * A command framework to support both normal string arguments and subcommands
@@ -41,14 +39,53 @@ public abstract class CommandFramework implements TabExecutor {
      * The Permission.
      */
     protected @Nullable String permission;
-    /**
-     * The Sub commands.
-     */
-    protected Dictionary<Integer, List<CommandFramework>> subCommands;
+
+    protected int argumentPosition = -1;
+
     /**
      * The Arguments.
      */
-    protected Dictionary<Integer, List<CommandArgument>> arguments;
+    protected Dictionary<Integer, List<Object>> arguments;
+
+    protected CommandFramework(String cmdName) {
+        this(cmdName, null);
+    }
+
+    protected CommandFramework(String cmdName, String description) {
+        this(cmdName, description, null);
+    }
+
+    protected CommandFramework(String cmdName, String description, List<String> aliases) {
+        this(cmdName, description, aliases, null);
+    }
+
+    protected CommandFramework(String cmdName, String description, List<String> aliases, String usageMessage) {
+        this(cmdName, description, aliases, null, null);
+    }
+
+    protected CommandFramework(String cmdName, String description, List<String> aliases, String usageMessage, @Nullable String permission) {
+        this(cmdName, description, aliases, usageMessage, null, null);
+    }
+
+    protected CommandFramework(@NotNull String cmdName, @Nullable String description, @Nullable List<String> aliases, @Nullable String usageMessage, @Nullable String permission, @Nullable int argumentPosition) {
+        this(cmdName, description, aliases, usageMessage, permission, -1, null);
+    }
+
+    protected CommandFramework(String cmdName, String description, List<String> aliases, String usageMessage, @Nullable String permission, Dictionary<Integer, List<Object>> arguments) {
+        this(cmdName, description, aliases, usageMessage, permission, -1, arguments);
+    }
+
+    protected CommandFramework(@NotNull String cmdName, @Nullable String description, @Nullable List<String> aliases,
+                               @Nullable String usageMessage, @Nullable String permission,
+                               int argumentPosition, @Nullable Dictionary<Integer, List<Object>> arguments) {
+        this.cmdName = cmdName;
+        this.description = description;
+        this.aliases = aliases;
+        this.usageMessage = usageMessage;
+        this.permission = permission;
+        this.argumentPosition = argumentPosition;
+        this.arguments = arguments;
+    }
 
     /**
      * Test if an audience has a permission node or not.
@@ -76,9 +113,65 @@ public abstract class CommandFramework implements TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (testPermission(sender)) {
+            int i = 0;
+            for (String arg : args) {
+                if (isSubCommand(arg)) {
+                    String[] newArgs = new String[args.length - (i + 1)];
+                    for (int j = 0; j + i < args.length; j++) {
+                        newArgs[j] = args[j + i];
+                    }
+                    return Objects.requireNonNull(getSubCommand(arg)).onCommand(sender, command, label, newArgs);
+
+                }
+
+                if (isCommandArg(arg)
+                        && !Objects.requireNonNull(getCommandArg(arg)).testPermission(sender)) {
+                    return false;
+                }
+                i++;
+            }
+
             return execute(sender, command, label, args);
         }
+
         return false;
+    }
+
+    private CommandArgument getCommandArg(String arg) {
+        Enumeration<List<Object>> args = this.arguments.elements();
+        while (args.hasMoreElements()) {
+            List<Object> argList = args.nextElement();
+            for (Object argument : argList) {
+                if (argument instanceof CommandArgument cmdArgument
+                        && Objects.equals(cmdArgument.getValue(), arg)) {
+                    return cmdArgument;
+                }
+            }
+        }
+        return null;
+    }
+
+    private boolean isCommandArg(String arg) {
+        return getCommandArg(arg) != null;
+    }
+
+    private CommandFramework getSubCommand(String cmdName) {
+        Enumeration<List<Object>> args = this.arguments.elements();
+        while (args.hasMoreElements()) {
+            List<Object> argList = args.nextElement();
+            for (Object argument : argList) {
+                if (argument instanceof CommandFramework cmdFramework
+                        && Objects.equals(cmdFramework.getCmdName(), cmdName)) {
+                    return cmdFramework;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private boolean isSubCommand(String cmdName) {
+        return getSubCommand(cmdName) != null;
     }
 
     /**
@@ -91,27 +184,27 @@ public abstract class CommandFramework implements TabExecutor {
      * @return the list
      */
     public List<String> tabComplete(CommandSender sender, Command command, String label, @NotNull String[] args) {
-        String arg = args[args.length - 1];
         List<String> tabList = new ArrayList<>();
 
-        // Subcommand Args
-        List<CommandFramework> subCommandArgs = subCommands.get(args.length);
+        // Value Args
+        List<Object> argValues = arguments.get(args.length);
 
-        if (subCommandArgs != null) {
-            for (CommandFramework subArg : subCommandArgs) {
-                if (subArg.getCmdName().startsWith(arg) && subArg.testPermission(sender)) {
-                    tabList.add(subArg.getCmdName());
-                }
-            }
+        if (args.length > 1 && isSubCommand(args[args.length - 2])) {
+            argValues.addAll(Objects.requireNonNull(getSubCommand(cmdName))
+                    .arguments.get(args.length - (args.length - 1)));
         }
 
-        // Value Args
-        List<CommandArgument> argValues = arguments.get(args.length);
-
         if (argValues != null) {
-            for (CommandArgument argument : argValues) {
-                if (argument.getValue().startsWith(arg) && argument.testPermission(sender)) {
-                    tabList.add(argument.getValue());
+
+            for (Object argument : argValues) {
+                if (argument instanceof CommandFramework cmdFramework) {
+                    tabList.add(cmdFramework.getCmdName());
+                } else if (argument instanceof CommandArgument cmdArgument) {
+                    tabList.add(cmdArgument.getValue());
+                } else if (argument instanceof String cmdString) {
+                    tabList.add(cmdString);
+                } else {
+                    tabList.add(argument.toString());
                 }
             }
         }
