44d12948afc83ee612eb4fe54ce8a14b1faadc5f
Solar404 <solar@lunar-dev.com>
2023-11-14 09:21:58 +0000

    ♻️ refactor: Moved old code to old folder for later use

--- src/main/java/com/lunardev/lunarlib/exceptions/NoCommandNameProvidedException.java
+++
@@ -1,7 +0,0 @@
-package com.lunardev.lunarlib.exceptions;
-
-public class NoCommandNameProvidedException extends Exception {
-    public NoCommandNameProvidedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/lunardev/lunarlib/exceptions/NoFreeSpaceException.java
+++
@@ -1,7 +0,0 @@
-package com.lunardev.lunarlib.exceptions;
-
-public class NoFreeSpaceException extends Exception {
-    public NoFreeSpaceException(String message) {
-        super(message);
-    }
-}
